c46ac30e60a272b54a595506edf62389dd029538
tbrekalo <tvrtko@optimoroute.com>
2022-09-29 17:21:20 +0200

    removed commented code

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -50,22 +50,6 @@ local on_attach = function(client, bufnr)
     lsp_signature.on_attach()
   end
 
-  -- local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  -- Highlighting references
-  -- if client.server_capabilities.document_highlight then
-  --   vim.api.nvim_exec([[
-  --     augroup lsp_document_highlight
-  --       autocmd! * <buffer>
-  --       autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-  --       autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-  --     augroup END
-  --   ]], false)
-  -- end
-
-  -- Enable completion triggered by <c-x><c-o>
-  -- buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
   -- Mappings.
   local opts = { noremap = true, silent = true }
 
