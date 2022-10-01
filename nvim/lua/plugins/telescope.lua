338d288aef7883ea4fe18f17483511922c637222
tbrekalo <tvrtko@optimoroute.com>
2022-10-01 12:11:27 +0200

    formatting

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ telescope.setup({
       fuzzy = true, -- false will only do exact matching
       override_generic_sorter = true, -- override the generic sorter
       override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case" or "smart_case"
+      case_mode = 'smart_case', -- or "ignore_case" or "respect_case" or "smart_case"
     },
   },
 })
