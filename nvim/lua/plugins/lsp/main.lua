0555d7354be96f7f5c091d702d58fdf94d295c6e
tbrekalo <tvrtko@optimoroute.com>
2022-10-07 14:03:23 +0200

    simplified lsp capabilities

--- nvim/lua/plugins/lsp/main.lua
+++ nvim/lua/plugins/lsp/main.lua
@@ -12,9 +12,7 @@ require('plugins.lsp.nvim-cmp')
 require('plugins.lsp.null-ls')
 require('plugins.lsp.lua-dev')
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
-
+local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local completionItem = capabilities.textDocument.completion.completionItem
 
 completionItem.documentationFormat = { 'markdown', 'plaintext' }
