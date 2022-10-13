741d6eec822907a49449ae6c743f479d087fff8a
tbrekalo <tvrtko@optimoroute.com>
2022-10-13 15:09:20 +0200

    refactored lsp config

--- nvim/lua/plugins/lsp/main.lua
+++ nvim/lua/plugins/lsp/main.lua
@@ -3,52 +3,62 @@ if not lsp_status_ok then
   return
 end
 
-local attach_keybingins = require('plugins.lsp.keybindings')
 local cmp = require('plugins.lsp.components.nvim-cmp')
 require('plugins.lsp.components.null-ls')
 require('plugins.lsp.components.lua-dev')
 
+local on_attach = require('plugins.lsp.attach')
 local capabilities = cmp.bind_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local on_attach = function(client, bufnr)
-  local lsp_signature_ok, lsp_signature = pcall(require, 'lsp_signature')
-  if lsp_signature_ok then
-    lsp_signature.on_attach({
-      bind = true,
-      handler_opts = {
-        border = 'rounded',
-      },
-      hint_prefix = '',
-    }, bufnr)
-  end
-
-  local opts = { noremap = true, silent = true, buffer = bufnr }
-  attach_keybingins(opts)
-
-  -- TODO: inject lsp specific keybingins
-  if client.name == 'clangd' then
-    vim.keymap.set('n', '<leader>o', '<cmd>ClangdSwitchSourceHeader<cr>', opts)
-  end
-end
-
-local servers = { 'sumneko_lua', 'pyright', 'clangd', 'tsserver', 'html', 'cssls' }
-local server_settings = {
-  ['sumneko_lua'] = require('plugins.lsp.servers.sumneko_lua'),
-}
-
--- Call setup
-for _, lsp in ipairs(servers) do
-  local args = {
+local configured = { 'pyright', 'tsserver', 'html', 'cssls' }
+for _, lsp in ipairs(configured) do
+  lspconfig[lsp].setup({
     on_attach = on_attach,
     capabilities = capabilities,
-    flags = {
-      -- default in neovim 0.7+
-      debounce_text_changes = 150,
-    },
-  }
-  if server_settings[lsp] then
-    args.settings = server_settings[lsp]
-  end
-
-  lspconfig[lsp].setup(args)
+  })
 end
+
+-- clangd
+lspconfig['clangd'].setup({
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    if client.name == 'clangd' then
+      vim.keymap.set(
+        'n',
+        '<leader>o',
+        '<cmd>ClangdSwitchSourceHeader<cr>',
+        { noremap = true, silent = true, buffer = bufnr }
+      )
+    end
+  end,
+  capabilities = capabilities,
+})
+
+local sumneko_settings = {
+  ['sumneko_lua'] = {
+    Lua = {
+      cmd = { 'lua-language-server' },
+      format = {
+        enable = false,
+      },
+      filetypes = { 'lua' },
+      runtime = {
+        version = 'LuaJIT',
+      },
+      completion = { enable = true, callSnippet = 'Both' },
+      diagnostics = {
+        enable = true,
+        globals = { 'vim', 'describe' },
+      },
+      workspace = {
+        library = {
+          vim.api.nvim_get_runtime_file('', true),
+        },
+        -- adjust these two values if your performance is not optimal
+        maxPreload = 2000,
+        preloadFileSize = 1000,
+      },
+      telemetry = { enable = false },
+    },
+  },
+}
