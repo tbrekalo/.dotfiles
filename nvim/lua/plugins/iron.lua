0611a1c2623abe35a884c4fc87d614ea5d9f36c3
tbrekalo <tvrtko@optimoroute.com>
2022-10-17 15:10:01 +0200

    reconfigured iron keybingings

--- nvim/lua/plugins/iron.lua
+++ nvim/lua/plugins/iron.lua
@@ -3,6 +3,11 @@ if not is_iron_ok then
   return
 end
 
+local map = function(m, k, v, opts)
+  opts = opts or { silent = true }
+  vim.keymap.set(m, k, v, opts)
+end
+
 iron.setup({
   config = {
     scratch_repl = true,
@@ -14,30 +19,12 @@ iron.setup({
     },
 
     repl_open_cmd = require('iron.view').split.vertical.botright(0.5),
-
-    keymaps = {
-      send_motion = '<leader>sc',
-      visual_send = '<leader>sc',
-      send_file = '<leader>sf',
-      send_line = '<leader>sl',
-      send_mark = '<leader>sm',
-      mark_motion = '<leader>mc',
-      mark_visual = '<leader>mc',
-      remove_mark = '<leader>md',
-      cr = '<leader>s<cr>',
-      interrupt = '<leader>s<leader>',
-      exit = '<leader>sq',
-      clear = '<leader>cl',
-    },
   },
 })
 
-local map = function(m, k, v, opts)
-  opts = opts or { silent = true }
-  vim.keymap.set(m, k, v, opts)
-end
-
-map('n', '<space>rs', '<cmd>IronRepl<cr>')
-map('n', '<space>rr', '<cmd>IronRestart<cr>')
-map('n', '<space>rf', '<cmd>IronFocus<cr>')
-map('n', '<space>rh', '<cmd>IronHide<cr>')
+map('n', '<leader>is', '<cmd>IronRepl<cr>')
+map('n', '<leader>ir', '<cmd>IronRestart<cr>')
+map('n', '<leader>if', '<cmd>IronFocus<cr>')
+map('n', '<leader>ih', '<cmd>IronHide<cr>')
+map('n', '<leader>il', iron.send_line)
+map('v', '<leader>s', iron.visual_send)
