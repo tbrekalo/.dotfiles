c7d464719105e3e3b8c6986d782c6076f55ffa95
tbrekalo <tvrtko@optimoroute.com>
2022-10-27 17:18:08 +0200

    update orgmode floating window config and edited templates

--- nvim/lua/plugins/orgmode.lua
+++ nvim/lua/plugins/orgmode.lua
@@ -16,32 +16,22 @@ end
 orgmode.setup_ts_grammar()
 orgmode.setup({
   org_default_notes_file = local_cfg.DEFAULT_NOTES_FILE,
-  win_split_mode = function(name)
-    local bufnr = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_name(bufnr, name)
-
-    local fill = 0.8
-    local width = math.floor((vim.o.columns * fill))
-    local height = math.floor((vim.o.lines * fill))
-    local row = math.floor((((vim.o.lines - height) / 2) - 1))
-    local col = math.floor(((vim.o.columns - width) / 2))
-
-    vim.api.nvim_open_win(bufnr, true, {
-      relative = 'editor',
-      width = width,
-      height = height,
-      row = row,
-      col = col,
-      style = 'minimal',
-      border = 'rounded',
-    })
-  end,
+  win_split_mode = 'float',
 
   org_capture_templates = {
+    t = {
+      description = 'Todo items',
+      template = '* TODO [/] %U: %?\n',
+      target = local_cfg.DEFAULT_TODO_FILE,
+    },
     j = {
       description = 'Journal entry',
-      template = '* %<%A %d-%m-%Y> log:\n** %?',
+      template = '* %<%A %d-%m-%Y> log:\n  - %?',
       target = local_cfg.DEFAULT_JOURNAL_FILE,
+    },
+    r = {
+      description = 'Revision question',
+      template = '* %U %? \n',
     },
   },
 })
